27eccd36025256635650f67e7afe6cd30275b19d
NishNilanka <nishan.nilanka@gmail.com>
2020-02-02 01:57:05 +0530

    Changed encoding patters

--- FFmpegWriter.cpp
+++ FFmpegWriter.cpp
@@ -3,7 +3,7 @@
 //frame writer is implemnted with OpenCV VideoWriter. Hence it is not possible to save the result file with the same encoding
 FFmpegWriter::FFmpegWriter(int width, int height, double FPS, string format)
 {
-		out.open("output.avi", CV_FOURCC('I','4','2','0'), FPS, Size(width, height/2));
+		out.open("output.mpeg4", CV_FOURCC('H','2','6','3'), FPS, Size(width, height/2));
 
 
 }
